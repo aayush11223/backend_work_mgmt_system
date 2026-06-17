b9b133f94092357c296928254552dc005b605d77
Aayush Basnet <aayush9823@gmail.com>
2026-06-17 19:55:00 +0545

    WorkLog has been added

--- routes/worklogs.js
+++ routes/worklogs.js
@@ -1,44 +1,57 @@
-// GET /worklogs — returns all worklogs
-// POST /worklogs — adds a new worklog
-
 const express = require('express');
-const fs = require('fs').promises;
-const path = require('path');
-
+const prisma = require('../db');
 const router = express.Router();
-const filePath = path.join(__dirname, '../data/worklogs.json');
 
+// GET /worklogs - find worklogs for the logged-in user
 router.get('/', (req, res) => {
-    fs.readFile(filePath, 'utf8')
-        .then((data) => res.status(200).json(JSON.parse(data)))
-        .catch((err) => {
-            console.error('Error reading attendance file:', err);
-            res.status(500).json({ error: 'Internal server error' });
-        });
-});
-
-router.post('/', (req, res) => {
-    fs.readFile(filePath, 'utf8')
-        .then((data) => {
-            const records = JSON.parse(data);
-
-            const newRecord = {
-                id: records.length + 1,
-                ...req.body, //adds up the value POST by user
-            };
-
-            records.push(newRecord);
-
-            return fs.writeFile(filePath, JSON.stringify(records))
-                //it permanently writes the POST data of a user in worklogs.json
-                .then(() => res.status(201).json(newRecord));
+    prisma.workLog.findMany({
+        where: { userId: parseInt(req.query.userId) },
+    })
+        .then((logs) => {
+            res.status(200).json(logs);
         })
         .catch((err) => {
-            console.error('Error updating attendance file:', err);
+            console.error('Error fetching worklogs:', err);
             res.status(500).json({ error: 'Internal server error' });
         });
 });
 
+// GET /worklogs/all - find all worklogs for admin
+router.get('/all', (req, res) => {
+    prisma.workLog.findMany({
+        include: {
+            user: {
+                select: { name: true },
+            },
+        },
+    })
+        .then((logs) => {
+            res.status(200).json(logs);
+        })
+        .catch((err) => {
+            console.error('Error fetching all worklogs:', err);
+            res.status(500).json({ error: 'Internal server error' });
+        });
+});
 
+// POST /worklogs - create a new worklog
+router.post('/', (req, res) => {
+    prisma.workLog.create({
+        data: {
+            userId: parseInt(req.body.userId),
+            date: (req.body.date),
+            description: req.body.description,
+            units: req.body.units,
+            hours: req.body.hours,
+        },
+    })
+        .then((log) => {
+            res.status(201).json(log);
+        })
+        .catch((err) => {
+            console.error('Error creating worklog:', err);
+            res.status(500).json({ error: 'Internal server error' });
+        });
+});
 
 module.exports = router;
